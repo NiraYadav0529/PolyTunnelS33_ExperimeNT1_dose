958f94015496bde61df6eaf5753b7e77005c2ace
NiraYadav0529 <90958427@westernsydney.edu.au>
2024-09-11 13:25:34 +1000

    new changes

--- ShootBiomass_First Fertilization_30daysafter_exp1.R
+++ ShootBiomass_First Fertilization_30daysafter_exp1.R
@@ -14,6 +14,8 @@ Plant_traits <- read.csv("Modified Data File/Biomass_height_stem_data_exp1.csv")
 # Load the dataset
 Plant_traits <- read.csv("Biomass_height_stem_data_exp1.csv")
 colnames(Plant_traits)
+
+
 # Ensure necessary columns are treated as factors
 Plant_traits <- Plant_traits %>%
   mutate(
@@ -29,15 +31,13 @@ Plant_traits %>%
 biomass_summary <- Plant_traits %>%
   group_by(Dose, Plant.type, Fertilizer_Type) %>%
   summarise(
-    Mean_Shoot = mean(Fresh.Biomass_harvestday..gm., na.rm = TRUE),
-    SE_Shoot = sd(Fresh.Biomass_harvestday..gm., na.rm = TRUE) / sqrt(n()),
-    Mean_Root = mean(Root.Biomass_harvestday.gm., na.rm = TRUE),
-    SE_Root = sd(Root.Biomass_harvestday.gm., na.rm = TRUE) / sqrt(n())
-  ) %>% ungroup()
+    Mean_Shoot = mean(Fresh_biomass_AF_30days.gm., na.rm = TRUE),
+    SE_Shoot = sd(Fresh_biomass_AF_30days.gm., na.rm = TRUE) / sqrt(n()),
+     ) %>% ungroup()
 
 ### SHOOT BIOMASS ANALYSIS ###
 # Model for shoot biomass
-m1_shoot <- lm(Fresh.Biomass_harvestday..gm. ~ Dose * Fertilizer_Type * Plant.type 
+m1_shoot <- lm(Fresh_biomass_AF_30days.gm. ~ Dose * Fertilizer_Type * Plant.type 
                - Dose:Fertilizer_Type:Plant.type, data = Plant_traits)
 summary(m1_shoot)
 
@@ -63,7 +63,7 @@ ggplot(biomass_summary_shoot, aes(x = Dose, y = Mean_Shoot, fill = Fertilizer_Ty
   geom_bar(stat = "identity", position = position_dodge(width = 0.9), colour = "black") +
   geom_errorbar(aes(ymin = Mean_Shoot - SE_Shoot, ymax = Mean_Shoot + SE_Shoot), 
                 width = 0.2, position = position_dodge(0.9)) +
-  labs(title = "Shoot Biomass by Dose and Fertilizer Type", x = "Dose (N kg/ha)", 
+  labs(title = "Shoot Biomass after First Fertilization", x = "Dose (N kg/ha)", 
        y = "Shoot Biomass (g, +/- SE)", fill = 'Fertilizer') +
   facet_wrap(~ Plant.type) +
   scale_fill_manual(values = c("UF" = "black", "MF" = "white", "None" = "grey")) +
@@ -71,38 +71,3 @@ ggplot(biomass_summary_shoot, aes(x = Dose, y = Mean_Shoot, fill = Fertilizer_Ty
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
   geom_text(aes(y = Mean_Shoot + SE_Shoot + 3, label = .group), 
             position = position_dodge(0.9))
-
-### ROOT BIOMASS ANALYSIS ###
-# Model for root biomass
-m1_root <- lm(Root.Biomass_harvestday.gm. ~ Dose * Fertilizer_Type * Plant.type, data = Plant_traits)
-summary(m1_root)
-
-# Check residuals for root biomass
-residualPlot(m1_root)
-qqPlot(m1_root)
-
-# Test significance of the model effects for root biomass
-Anova(m1_root, type = "II")
-
-# Perform multiple comparison test (Tukey HSD) for root biomass
-pairwise_comparisons_root <- emmeans(m1_root, ~ Dose | Plant.type)
-letters_root <- cld(pairwise_comparisons_root) %>%
-  as.data.frame()
-
-# Merge the letters for root biomass with biomass_summary using Dose and Plant.type
-biomass_summary_root <- biomass_summary %>%
-  left_join(letters_root, by = c("Dose", "Plant.type"))
-
-# ROOT BIOMASS PLOT
-ggplot(biomass_summary_root, aes(x = Dose, y = Mean_Root, fill = Fertilizer_Type)) +
-  geom_bar(stat = "identity", position = position_dodge(width = 0.9), colour = "black", alpha = 0.5) +
-  geom_errorbar(aes(ymin = Mean_Root - SE_Root, ymax = Mean_Root + SE_Root), 
-                width = 0.2, position = position_dodge(0.9)) +
-  labs(title = "Root Biomass by Dose and Fertilizer Type", x = "Dose (N kg/ha)", y = "Root Biomass (gm)") +
-  facet_wrap(~ Plant.type) +
-  scale_fill_manual(values = c("UF" = "black", "MF" = "white", "None" = "grey")) +
-  theme_minimal() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-  geom_text(aes(y = Mean_Root + SE_Root + 0.5, label = .group), 
-            position = position_dodge(0.9), vjust = -0.5)
-
